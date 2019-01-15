3c4e47e1decd96d0969427b3bacaba3a17d055b7
cla <chonla4@yahoo.com>
2019-01-14 17:04:25 -0700

    first commit

---
+++ src/main/java/com/exactuals/studio/en/EnApplication.java
@@ -0,0 +1,14 @@
+package com.exactuals.studio.en;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnApplication.class, args);
+	}
+
+}
+
